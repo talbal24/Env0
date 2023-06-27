5f8d932c604d6c3b7bc93e9e4ad65e96aa33b27f
talbal24 <134201605+talbal24@users.noreply.github.com>
2023-06-27 15:57:46 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAT2AJC3K6FFZVRHF2"
-  secret_key = "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"
-}  
-
 resource "aws_instance" "myec2" {
     ami = "ami-00c39f71452c08778"
     instance_type = "t2.micro"
